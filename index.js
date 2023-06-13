1f5c9ac2040d2b93012bc4b0bbea6f9abce79563
**** <****>
2023-06-13 14:34:42 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute('id','victory');
+document.body.appendChild(newHeader);
+
+
+const newName= document.getElementById('victory');
+newHeader.textContent = "Felix is the champion";
